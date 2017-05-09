a9a1964a482add5d5716c30e810fd7f6963c3909
Hano-CM\Hano <hano.prinsloo@gmail.com>
2017-05-09 19:39:29 +0200

    Second Commit

--- app/src/main/java/com/example/busby/busbyapp/Main2Activity.java
+++ app/src/main/java/com/example/busby/busbyapp/Main2Activity.java
@@ -8,6 +8,6 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.login_screen);
     }
 }
