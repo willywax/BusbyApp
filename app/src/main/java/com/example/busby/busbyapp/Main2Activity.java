f64eb27edd4e7917b4eb60f800a1d3fd86dde4e6
Hano-CM\Hano <hano.prinsloo@gmail.com>
2017-05-14 00:27:14 +0200

    landing and transition back end. Will do commit more tomorrow

--- app/src/main/java/com/example/busby/busbyapp/Main2Activity.java
+++ app/src/main/java/com/example/busby/busbyapp/Main2Activity.java
@@ -2,12 +2,21 @@ package com.example.busby.busbyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.landing);
+
+        final Button button = (Button) findViewById(R.id.firstLoginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.login_screen);
+            }
+        });
     }
 }
